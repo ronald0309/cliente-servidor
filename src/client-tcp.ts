8eb6d1f39700e8c3f455dde28eac20d39d6eae88
ronald0309 <42876234+ronald0309@users.noreply.github.com>
2021-04-16 19:37:10 -0600

    Mejorando nuestro cliente

--- src/client-tcp.ts
+++ src/client-tcp.ts
@@ -25,8 +25,12 @@ ws.onmessage = function (event: any) {
 
   stdin.addListener('data', function (d) {
     const message = d.toString().trim();
- 
-      ws.send(message);
+        if(message=='#salir'){
+            ws.send('Adios servidor!');
+            ws.close();}
+        else{
+            ws.send(message);
+        }
    
   });
 }
